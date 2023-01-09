434d5a1a0b63160538d0b14017bcb78e17493481
Christopher Cialone <ccialone0929@gmail.com>
2023-01-09 07:14:31 -0500

    Added Cors Origin All set to true

--- squaring_api/squaring_api/settings.py
+++ squaring_api/squaring_api/settings.py
@@ -46,12 +46,13 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'squaring_api.urls'
 
